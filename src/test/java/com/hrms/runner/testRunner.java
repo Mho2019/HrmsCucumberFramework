bbaef11f82184c7ceb69b861d900e60764767081
Murad <murad_hoss@yahoo.com>
2021-02-19 22:38:05 -0500

    POM.xml were added

--- src/test/java/com/hrms/runner/testRunner.java
+++ src/test/java/com/hrms/runner/testRunner.java
@@ -8,7 +8,11 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(features=".\\src\\test\\resources\\Features\\"
 ,glue="com.hrms.Steps"
-,tags= {"@Login"}
+,tags= {"@Login"},
+plugin= {
+		"pretty",//prints gherkin steps in console
+		"html:target/cucumber-default-report",//create basic html report in specified location
+		"json:target/cucumber.json"}
 		)
 public class testRunner {
 
